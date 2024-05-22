6fa0cb27bd7d9e4c10c450a9a61ced53dcaa1176
Serzc <serzc.cr@gmail.com>
2024-05-22 14:32:39 -0600

    tiempos cambiados

--- proyecto1.v
+++ proyecto1.v
@@ -92,11 +92,11 @@ module overflowR(a,b,s,sig_O);
     //si a[31] y b[31] son 0, ambos A y B son positivos. Si se suman y da negativo (s[31] = 1) entonces hay overflow
     //(a[31]*b[31]*s[31]')+(a[31]'*b[31]'*s[31])
     not #(1) g_not2 (c00,s[31]);
-    and #(2) g_and4 (c01,a[31],b[31],c00);
+    and #(3) g_and4 (c01,a[31],b[31],c00);
     not #(1) g_not3 (c02,a[31]);
     not #(1) g_not4 (c03,b[31]);
-    and #(2) g_and5 (c04,s[31],c02,c03);
-    or  #(2) g_or2  (sig_O,c04,c01);
+    and #(3) g_and5 (c04,s[31],c02,c03);
+    or  #(1) g_or2  (sig_O,c04,c01);
 endmodule
 
 
