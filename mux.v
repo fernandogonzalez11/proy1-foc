d071fb190908192e7c126f68acb858c8e239fe23
Quirkio <jose.ignacio.madriz@gmail.com>
2024-05-24 13:59:14 -0600

    Update mux.v

--- mux.v
+++ mux.v
@@ -1,21 +1,16 @@
-`timescale 100ps / 100ps
+module mux_4x1(input s0, s1, a, b, c, d
+                output  Y )
 
-module mux4x1(input[3:0] a, 
-              input[3:0] b, 
-              input[3:0] c, 
-              input[3:0] d, 
-              input[1:0] sel, 
-              output reg [3:0]out, 
-)
+wire notS0, notS1, out1, out2, out3, out4;
 
-always @ (a or b or c or d or sel) 
-begin
-    case(sel)
-    2'b00: out <= a;
-    2'b01: out <= b;
-    2'b10: out <= c;
-    2'b11: out <= d;
-    endcase
-end
+not not1(notS0, s0);
+not not2(notS1, s1);
+
+and and1 (out1, a, notS1, notS0);
+and and2(out2, b, notS1, s0);
+and and3(out3, c, s1, notS0);
+and and4(out4, d, s1, s0);
+
+or or1(Y, out1, out2, out3, out4);
 
 endmodule
