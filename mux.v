0ae30fddbe7778f6b37f4e0f6e1aba5ffb77a7ca
fernandogonzalez11 <fernandogonzalezeducativo@gmail.com>
2024-05-24 18:23:38 -0600

    cambio de ubicación del overflow testbench de unit A con pruebas de overflow hacer el mux compilable

--- mux.v
+++ mux.v
@@ -1,16 +1,17 @@
-module mux_4x1(input s0, s1, a, b, c, d
-                output  Y )
+module mux_4x1(s, a, b, c, d, Y);
+    input [1:0] s;
+    input a, b, c, d;
+    output Y;
+    wire notS0, notS1, out1, out2, out3, out4;
 
-wire notS0, notS1, out1, out2, out3, out4;
+    not not1(notS0, s0);
+    not not2(notS1, s1);
 
-not not1(notS0, s0);
-not not2(notS1, s1);
+    and and1(out1, a, notS1, notS0);
+    and and2(out2, b, notS1, s0);
+    and and3(out3, c, s1, notS0);
+    and and4(out4, d, s1, s0);
 
-and and1 (out1, a, notS1, notS0);
-and and2(out2, b, notS1, s0);
-and and3(out3, c, s1, notS0);
-and and4(out4, d, s1, s0);
-
-or or1(Y, out1, out2, out3, out4);
+    or or1(Y, out1, out2, out3, out4);
 
 endmodule
--- testbenches/unit_A_tb.v
+++ testbenches/unit_A_tb.v
@@ -1,33 +1,53 @@
 `include "unit_A.v"
+`timescale 100ps / 100ps
 
 module unit_A_testbench;
     reg [31:0] A, B;
     reg [3:0] f;
     wire [31:0] S;
-    wire c_out;
+    wire c_out, O;
 
-    unit_A uA(A, B, f, S, c_out);
+    unit_A uA(A, B, f, S, c_out, O);
 
     initial begin
         $dumpfile("out/unit-A-dump.vcd");
         $dumpvars(0, unit_A_testbench);
 
+        // pruebas de operaciones y carry out
         $display("A = 6, B = 6, f = 0100 (sum)");
         A = 32'h00000006;
         B = 32'h00000006;
         f = 4'b0100;
 
-        #150;
-        $display("A = 6, B = 6, f = 0101 (sub)");
+        #200;
+        $display("A = 6, B = 6, f = 0101 (sub) -> c_out = 1");
         f = 4'b0101;
 
-        #150
+        #200
         $display("A = 0, B = 6, f = 0110 (ainv)");
         A = 32'h00000000;
         f = 4'b0110;
 
-        #150
+        #200
         $display("A = 0, B = 6, f = 0111 (inc)");
         f = 4'b0111;
+
+        // pruebas de overflow
+        #200
+        $display("A = +MAX, B = 1, f = 0100 (sum) -> overflow");
+        A = 32'h7FFFFFFF;
+        B = 32'h00000001;
+        f = 4'b0100;
+
+        #200
+        $display("A = +MAX, B = 1, f = 0101 (sub)");
+        f = 4'b0101;
+
+        #200
+        $display("A = +MAX, B = -1, f = 0101 (sub) -> overflow");
+        B = 32'hFFFFFFFF;
+
+        #200
+        $finish;
     end
 endmodule
